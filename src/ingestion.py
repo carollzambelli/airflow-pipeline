e0a8dbcac78359f116e147fe7877738705f95bc2
Carolina <caroll.zambelli@gmail.com>
2024-05-10 20:39:21 -0300

    first commit

---
+++ src/ingestion.py
@@ -0,0 +1,54 @@
+"Script de ingestão treinamento DPH"
+
+import pandas as pd
+import requests
+import utils
+import os
+from dotenv import load_dotenv
+from config import configs
+import logging
+
+load_dotenv()
+config_file = configs
+logging.basicConfig(level=logging.INFO)
+
+
+def ingestion():
+    """
+    Função de ingestão dos dados
+    Outputs: Salva base raw em local específico e retorna o nome do arquivo
+    """
+
+    logging.info("Iniciando a ingestão")
+    api_url = os.getenv('URL')
+    try:
+        response = requests.get(api_url, timeout=10).json()
+        data = response['results']
+    except Exception as exception_error:
+        utils.error_handler(exception_error, 'read_api', configs['path']['logs'])
+    df = pd.json_normalize(data)
+    utils.save_folder(df, configs['path']['raw'])
+    
+
+def preparation():
+    """
+    Função de preparação dos dados: renomeia, tipagem, normaliza strings
+    Arguments: file -> nome do arquivo raw
+    Outputs: Salva base limpa em local específico
+    """
+
+    logging.info("Iniciando o saneamento")
+    file = f"{configs['path']['raw']}"
+    df = pd.read_csv(file, sep=";")
+    san = utils.Saneamento(df, config_file)
+    san.select_rename()
+    logging.info("Seleção de dados")
+    df = san.tipagem()
+    logging.info("Tipagem dos dados")
+    utils.save_folder(df, configs['path']['work'])
+    logging.info("Dados salvos na work")
+    
+
+if __name__ == '__main__':
+    ingestion()
+    preparation()
---
+++ src/utils.py
@@ -0,0 +1,48 @@
+import os
+import pandas as pd
+from datetime import datetime
+
+
+class Saneamento:
+    
+    def __init__(self, data, configs):
+        self.data = data
+        self.colunas = configs["metadados"]['nome_original']
+        self.colunas_new = configs["metadados"]['nome']
+        self.tipos = configs["metadados"]['tipos']  
+
+    def select_rename(self):
+        self.data = self.data.loc[:, self.colunas] 
+        for i in range(len(self.colunas)):
+            self.data.rename(columns={self.colunas[i]:self.colunas_new[i]}, inplace = True)
+
+    def tipagem(self):
+        for col in self.colunas_new:
+            tipo = self.tipos[col]
+            if tipo == "int":
+                tipo = self.data[col].astype(int)
+            elif tipo == "float":
+                self.data[col].replace(",", ".", regex=True, inplace = True)
+                self.data[col] = self.data[col].astype(float)
+            elif tipo == "date":
+                self.data[col] = pd.to_datetime(self.data[col]).dt.strftime('%Y-%m-%d')
+        return self.data
+    
+def save_folder(data, path):
+    data['load_date'] = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+    if not os.path.exists(path):
+        data.to_csv(path, index=False, sep = ";")
+    else:
+        data.to_csv(path, index=False, mode='a', header=False, sep = ";")
+
+
+def error_handler(exception_error, stage, path):
+    
+    log = [stage, type(exception_error).__name__, exception_error,datetime.now()]
+    logdf = pd.DataFrame(log).T
+    
+    if not os.path.exists(path):
+        logdf.columns = ['stage', 'type', 'error', 'datetime']
+        logdf.to_csv(path, index=False,sep = ";")
+    else:
+        logdf.to_csv(path, index=False, mode='a', header=False, sep = ";")
